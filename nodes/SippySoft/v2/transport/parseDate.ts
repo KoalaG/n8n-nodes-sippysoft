3b977a9b857f8120d3ed6decabb2480609487bb2
GavinJ <gavin@lightningip.com.au>
2023-09-18 13:55:25 +1000

    fixup date parsing

--- nodes/SippySoft/v2/transport/parseDate.ts
+++ nodes/SippySoft/v2/transport/parseDate.ts
@@ -1,3 +1,19 @@
+import { getNodeWebhookPath } from "n8n-workflow/dist/NodeHelpers";
+
+const monthMap: { [key: string]: string} = {
+	Jan: '01',
+	Feb: '02',
+	Mar: '03',
+	Apr: '04',
+	May: '05',
+	Jun: '06',
+	Jul: '07',
+	Aug: '08',
+	Sep: '09',
+	Oct: '10',
+	Nov: '11',
+	Dec: '12',
+};
 
 export function parseDate(
 		// Date string in format %H:%M:%S.000 GMT %a %b %d %Y
@@ -8,10 +24,10 @@ export function parseDate(
 		/(\d{2}):(\d{2}):(\d{2})\.000 GMT (\w{3}) (\w{3}) (\d{1,2}) (\d{4})/
 	);
 
-	const parsedDate = new Date(
-		`${dateMatch![5]} ${dateMatch![4]} ${dateMatch![7]} ${dateMatch![1]}:${dateMatch![2]}:${dateMatch![3]}`
-	);
+	if (!dateMatch) throw new Error(`Invalid date string: ${input}`);
 
+	const isoString = `${dateMatch[7]}-${monthMap[dateMatch[5]]}-${dateMatch[6].padStart(2,'0')}T${dateMatch[1]}:${dateMatch[2]}:${dateMatch[3]}.000Z`;
+	const parsedDate = new Date(isoString);
 
 	return parsedDate;
 }
